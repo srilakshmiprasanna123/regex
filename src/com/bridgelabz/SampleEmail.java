6dd89c99693d50b88b0d8369851089e9ee607291
srilakshmiprasanna123 <prasanna98ece.rymec@gmail.com>
2022-04-01 21:36:00 +0530

    added sample email

--- src/com/bridgelabz/SampleEmail.java
+++ src/com/bridgelabz/SampleEmail.java
@@ -3,17 +3,43 @@ package com.bridgelabz;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class PhoneNumber {
+public class SampleEmail {
 
     static String regexNamePattern="^[A-Z][a-zA-Z]{2,}$";
-    static String regexEmailPattern="^[A-Za-z+_.-]+@[A-Za-z.-]+$";
+    static String regexEmailPattern="^[A-Za-z0-9+_.-]+@[A-Za-z.-]+$";
     static String regexPhoneNumberPattern="^[0-9]{2}[\s]?[0-9]{10}$";
+    static String regexPasswordPattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&])[A-Za-z0-9@$!%*?&]{8,}$";
+
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Program");
         firstNameCheck();
         secondNameCheck();
         emailCheck();
         phoneNumberCheck();
+        passwordCheck();
+        String[] emailArray= {"abc@yahoo.com","abc-100@gmail.co.in","abc()*@gmail.com","abc@%*.com"};
+        emailSamplerCheck(emailArray);
+    }
+
+    private static void emailSamplerCheck(String[] mail0) {
+        for(String m: mail0) {
+            boolean check = Pattern.matches(regexEmailPattern, m);
+            if (check)
+                System.out.println("Email-ID is Validated");
+            else
+                System.out.println("Incorrect Email-ID,Try Again");
+        }
+    }
+
+    private static void passwordCheck() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the Valid Password: " );
+        String  pwd = s.next();
+        boolean check = Pattern.matches(regexPasswordPattern, pwd);
+        if (check)
+            System.out.println("Phone Number is Validated");
+        else
+            System.out.println("Incorrect Phone Number,Try Again");
     }
 
     private static void phoneNumberCheck() {
