1af3956ed8b7ef0b00c45c67ba4d3bb1f1edc895
srilakshmiprasanna123 <prasanna98ece.rymec@gmail.com>
2022-04-01 20:47:13 +0530

    added email

--- src/com/bridgelabz/Email.java
+++ src/com/bridgelabz/Email.java
@@ -3,13 +3,27 @@ package com.bridgelabz;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class LastName {
+public class Email {
 
     static String regexNamePattern="^[A-Z][a-zA-Z]{2,}$";
+    static String regexEmailPattern="^[A-Za-z+_.-]+@[A-Za-z.-]+$";
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Program");
         firstNameCheck();
         secondNameCheck();
+        emailCheck();
+    }
+
+    private static void emailCheck() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the Valid Email-ID: " );
+        String  mail = s.next();
+        boolean check = Pattern.matches(regexEmailPattern, mail);
+        if (check)
+            System.out.println("Email-ID is Validated");
+        else
+            System.out.println("Incorrect Email-ID,Try Again");
+
     }
 
     private static void secondNameCheck() {
@@ -23,7 +37,7 @@ public class LastName {
             System.out.println("Incorrect Last Name,Try Again");
     }
 
-    public static void firstNameCheck(){
+    private static void firstNameCheck(){
         Scanner s = new Scanner(System.in);
         System.out.print("Enter the Valid First Name: " );
         String  fName = s.next();
